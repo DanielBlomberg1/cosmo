1d8286c9c499304de5b825efe4b135bb240c4f7b
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-01-28 14:09:54 +0200

    update gitignore refactor main

--- CosmoEngine/-/Source/WinMain.cpp
+++ CosmoEngine/-/Source/WinMain.cpp
@@ -3,37 +3,51 @@
 #define MAX_NAME_SZ 256
 #define HInstance() GetModuleHandle(NULL)
 
-WCHAR WindowClass[MAX_NAME_SZ];
-WCHAR WindowTitle[MAX_NAME_SZ];
+#pragma region
+	WCHAR WindowClass[MAX_NAME_SZ];
+	WCHAR WindowTitle[MAX_NAME_SZ];
 
-INT WinWidth;
-INT WinHeight;
+	INT WinWidth;
+	INT WinHeight;
 
-HICON hIcon;
+	HICON hIcon;
+#pragma endregion
 
-LRESULT CALLBACK WProcess(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam) 
-{
-	switch (msg) {
-		case WM_DESTROY:
-			PostQuitMessage(0);
-			break;
-	}
 
-	return DefWindowProc(hWnd, msg, wparam, lparam);
-}
+
+#pragma region Pre-Declarations
+VOID InitializeVariables();
+VOID CreateWindowClass();
+VOID MessageLoop();
+VOID WindowInit();
+LRESULT CALLBACK WProcess(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam);
+#pragma endregion
 
 
 int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT )
 {
-	// globals
+	InitializeVariables();
+	CreateWindowClass();
+	WindowInit();
+	MessageLoop();
+	return 0;
+}
+
+// FUNCTIONS //
+#pragma region Func
+VOID InitializeVariables()
+{
 	LoadString(HInstance(), IDS_PERGAMENAME, WindowClass, MAX_NAME_SZ);
 	LoadString(HInstance(), IDS_WINDOWTITLE, WindowTitle, MAX_NAME_SZ);
 	WinWidth = 1920;
 	WinHeight = 1080;
 
 	hIcon = LoadIcon(HInstance(), MAKEINTRESOURCE(IDI_MAINICON));
+}
 
-	// window class
+
+VOID CreateWindowClass() 
+{
 	WNDCLASSEX wex;
 	wex.cbSize = sizeof(WNDCLASSEX);
 	wex.style = CS_HREDRAW | CS_VREDRAW;
@@ -48,27 +62,27 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT )
 
 	wex.lpszClassName = WindowClass;
 	wex.lpszMenuName = nullptr;
-	
+
 	wex.hInstance = HInstance();
 
 	wex.lpfnWndProc = WProcess;
 	RegisterClassEx(&wex);
+}
 
-	// Window
-
+VOID WindowInit() 
+{
 	HWND hWnd = CreateWindow(WindowClass, WindowTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, 0, WinWidth, WinHeight, nullptr, nullptr, HInstance(), nullptr);
 	if (!hWnd) {
 		MessageBeep(0);
 		MessageBox(0, L"Window Creation Failed", 0, 0);
-		return 0;
+		return;
 	}
-
 	ShowWindow(hWnd, SW_SHOW);
+}
 
-	// Looping Function
-
+VOID MessageLoop() {
 	MSG msg{ 0 };
-	while (msg.message != WM_QUIT) 
+	while (msg.message != WM_QUIT)
 	{
 		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
@@ -76,6 +90,16 @@ int CALLBACK WinMain(HINSTANCE, HINSTANCE, LPSTR, INT )
 			DispatchMessage(&msg);
 		}
 	}
-
-	return 0;
 }
+
+LRESULT CALLBACK WProcess(HWND hWnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+
+	return DefWindowProc(hWnd, msg, wparam, lparam);
+}
+#pragma endregion
