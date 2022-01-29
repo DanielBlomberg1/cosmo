db1d4ea06bd8e18c368553f0c99e4c589cee8c84
DAN-KOTI\Daniel <daniel.blombergof@gmail.com>
2022-01-29 12:13:27 +0200

    add engine modes

--- CosmoEngine/Cosmo/Source/Engine/OEngine.cpp
+++ CosmoEngine/Cosmo/Source/Engine/OEngine.cpp
@@ -1,1 +1,19 @@
 #include "Cosmo.h"
+
+namespace Engine {
+	OEngine gKosmoEngine;
+}
+
+OEngine::OEngine()
+{
+#ifdef _DEBUG
+	EM = EngineMode::DEBUG;
+#else
+	EM = EngineMode::RELEASE;
+#endif
+
+}
+
+OEngine::~OEngine()
+{
+}
--- CosmoEngine/Cosmo/Source/Engine/OEngine.h
+++ CosmoEngine/Cosmo/Source/Engine/OEngine.h
@@ -1,1 +1,24 @@
 #pragma once
+
+class COSMO_API OEngine;
+namespace Engine {
+	enum class EngineMode : INT {
+		NONE,
+		DEBUG,
+		RELEASE,
+		EDITOR,
+		SERVER
+	};
+
+	extern OEngine gKosmoEngine;
+}
+
+
+using namespace Engine;
+class COSMO_API OEngine {
+public:
+	OEngine();
+	~OEngine();
+private:
+	EngineMode EM;
+};
